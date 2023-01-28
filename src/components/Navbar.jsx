761288e341dcd0af26e44aa11e06832a7ad7f600
Niraj Kumar Tiwari <nirajkumar199731@gmail.com>
2023-01-28 22:37:01 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { SearchBar } from "./";
 import './logo.css';
 const Navbar = () => (
   <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
-    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+    <Link to="/YourTube" style={{ display: "flex", alignItems: "center" }}>
       <img src={logo} alt="logo" height={45} />
       <h2 className="logo-heading" style={{color:'white',paddingLeft:'2px'}}>YourTube</h2>
     </Link>
