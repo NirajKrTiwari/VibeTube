b21f52ff281b62edce4abd050b3865016f0929d1
Niraj Kumar Tiwari <nirajkumar199731@gmail.com>
2023-01-29 15:06:51 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => (
   <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
     <Link to="/YourTube" style={{ display: "flex", alignItems: "center" }}>
       <img src={logo} alt="logo" height={45} />
-      <h2 className="logo-heading" style={{color:'white',paddingLeft:'2px'}}>YourTube</h2>
+      <h2 className="logo-heading" style={{color:'white',paddingLeft:'6px',wordSpacing:'1px',fontFamily:''}}>YourTube</h2>
     </Link>
     <SearchBar />
   </Stack>
