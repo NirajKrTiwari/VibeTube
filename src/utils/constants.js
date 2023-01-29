2850de66683910f0defd97d62d050ef2234410f7
Niraj Kumar Tiwari <nirajkumar199731@gmail.com>
2023-01-29 15:07:03 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -11,32 +11,37 @@ import GraphicEqIcon from '@mui/icons-material/GraphicEq';
 import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
 import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
-
+import JavascriptIcon from '@mui/icons-material/Javascript';
+import DataObjectIcon from '@mui/icons-material/DataObject';
 export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
 
 export const categories = [
   { name: 'New', icon: <HomeIcon />, },
-  // { name: 'JS Mastery', icon: <CodeIcon />, },
-  { name: 'Coding', icon: <CodeIcon />, },
-  { name: 'ReactJS', icon: <CodeIcon />, },
+  { name: 'Live', icon: <LiveTvIcon />, },
+  { name: 'JS Mastery', icon: <JavascriptIcon />, },
+  { name: 'Code With Harry', icon: <CodeIcon />, },
+  { name: 'CodeHelp - by Babbar ', icon: <DataObjectIcon />, },
+  { name: 'Appna College', icon: <CodeIcon />, },
+  { name: 'ReactJS', icon: <JavascriptIcon />, },
   { name: 'NextJS', icon: <CodeIcon />, },
   { name: 'Music', icon: <MusicNoteIcon /> },
+  { name: 'News', icon: <LiveTvIcon />, },
   { name: 'Education', icon: <SchoolIcon />, },
-  { name: 'Podcast', icon: <GraphicEqIcon />, },
+  // { name: 'Podcast', icon: <GraphicEqIcon />, },
+  { name: 'Crypto', icon: <DeveloperModeIcon />, },
   { name: 'Movie', icon: <OndemandVideoIcon />, },
-  { name: 'Gaming', icon: <SportsEsportsIcon />, },
-  { name: 'Live', icon: <LiveTvIcon />, },
+  { name: 'Pubg', icon: <SportsEsportsIcon />, },
   { name: 'Sport', icon: <FitnessCenterIcon />, },
   { name: 'Fashion', icon: <CheckroomIcon />, },
-  { name: 'Beauty', icon: <FaceRetouchingNaturalIcon />, },
+  // { name: 'Beauty', icon: <FaceRetouchingNaturalIcon />, },
   { name: 'Comedy', icon: <TheaterComedyIcon />, },
   { name: 'Gym', icon: <FitnessCenterIcon />, },
-  { name: 'Crypto', icon: <DeveloperModeIcon />, },
+
 ];
 
-export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
+export const demoThumbnailUrl = 'https://www.howtogeek.com/wp-content/uploads/2021/08/YouTube-logo-hero-1.png?width=1198&trim=1,1&bg-color=000&pad=1,1';
 export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
 export const demoVideoUrl = '/video/GDa8kZLNhJ4';
-export const demoChannelTitle = 'JavaScript Mastery';
-export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
+export const demoChannelTitle = 'YourTube';
+export const demoVideoTitle = '......';
 export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
