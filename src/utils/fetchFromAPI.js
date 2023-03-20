7258cc76a7456899e4ec40ed01eaee8d385abe2f
Niraj Kumar Tiwari <nirajkumar199731@gmail.com>
2023-03-20 21:12:12 +0530

    Update fetchFromAPI.js

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -7,7 +7,9 @@ const options = {
     maxResults: 50,
   },
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    
+    // 'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key': 'c60d1ce272msh8cee2a3233837d7p197295jsn9e4e4c116476',
     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
   },
 };
